deff4d1b6939465dfaf9915fb8039cc0fd6db638
Abhishek-kumar09 <abhimait1909@gmail.com>
2022-05-07 16:57:50 +0530

    Complete the hero section
    Signed-off-by: Abhishek-kumar09 <abhimait1909@gmail.com>

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,21 +1,36 @@
+import styled from "@emotion/styled";
 import { Container, TextField, Typography } from "@mui/material";
+import CustomizedSearchBase from "./searchBox";
+
+const Header = styled('header')({
+  backgroundImage: `url("https://images.pexels.com/photos/8775535/pexels-photo-8775535.jpeg?auto=compress&cs=tinysrgb&fit=crop&fp-y=0.5&h=500&sharp=10&w=1400")`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  width: "100%",
+  height: "100%",
+  '&:before': {
+    backgroundColor: "rgba(0,0,0,0.4)",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: "20vh",
+    content: "no-open-quote"
+  }
+});
 
 export default function HeroComponent() {
   return (
-    <header className="App-header" style={{
-      backgroundImage: `url("https://images.pexels.com/photos/8775535/pexels-photo-8775535.jpeg?auto=compress&cs=tinysrgb&fit=crop&fp-y=0.5&h=500&sharp=10&w=1400")`,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      width: "100%",
-      height: "100%",
-    }}>
-      {/* <img src="https://images.pexels.com/photos/8775535/pexels-photo-8775535.jpeg?auto=compress&cs=tinysrgb&fit=crop&fp-y=0.5&h=500&sharp=10&w=1400"></img> */}
-      <Container>
-        <Typography variant="h3">
+    <Header className="App-header">
+      <Container style={{ zIndex: 2 }} >
+        <Typography variant="h3" fontWeight={600}>
           The best free stock photos, royalty free images & videos shared by creators.
         </Typography>
-        <TextField fullWidth />
+        <CustomizedSearchBase placeholder="Search for Free Photos and videos" />
+        <Typography variant="caption" textAlign={"left"} display="block" p={"8px 0px"}>
+          Suggested: mothers day, landscape, forest, flowers, nature mother more
+        </Typography>
       </Container>
-    </header>
+    </Header>
   )
 }
